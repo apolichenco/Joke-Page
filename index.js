9e1aa0fcf13731d3e8bda6b68d4936e4949a5bb1
abraham <apolichenco@gmail.com>
2022-06-21 23:03:35 -0400

    50 jokes

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?type=single&amount=10")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function (data) {
 
-        breedArray = Object.entries(data.jokes);
-        breedArray.forEach(link => {
-        categorize(link)
+function fetchJokes() {
+    fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?type=single&amount=10")
+        .then(function(response) {
+            return response.json();
         })
-    });
+        .then(function (data) {
 
-    function categorize(theJoke) {
-        if (theJoke[1].category === "Programming") {
-            findCorrectCategory("Programming", theJoke)
-        }
-        if (theJoke[1].category === "Misc") {
-            findCorrectCategory("Misc", theJoke)
-        }
-        if (theJoke[1].category === "Dark") {
-            findCorrectCategory("Dark", theJoke)
-        }
-        if (theJoke[1].category === "Pun") {
-            findCorrectCategory("Pun", theJoke)
-        }
-        if (theJoke[1].category === "Spooky") {
-            findCorrectCategory("Spooky", theJoke)
-        }
+            breedArray = Object.entries(data.jokes);
+            breedArray.forEach(link => {
+            categorize(link)
+            })
+        });
     }
+fetchJokes();
+fetchJokes();
+fetchJokes();
+fetchJokes();
+fetchJokes();
+
+function categorize(theJoke) {
+    if (theJoke[1].category === "Programming") {
+        findCorrectCategory("Programming", theJoke)
+    }
+    if (theJoke[1].category === "Misc") {
+        findCorrectCategory("Misc", theJoke)
+    }
+    if (theJoke[1].category === "Dark") {
+        findCorrectCategory("Dark", theJoke)
+    }
+    if (theJoke[1].category === "Pun") {
+        findCorrectCategory("Pun", theJoke)
+    }
+    if (theJoke[1].category === "Spooky") {
+        findCorrectCategory("Spooky", theJoke)
+    }
+}
 
 function findCorrectCategory(whichcategory, theJoke) {
         const jokeCategory = document.querySelector(`#${whichcategory}-jokes`)
